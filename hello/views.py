8ef2dabd94864589b0af0bd6ad4ddaa372a1cf36
André Nunes <andre.eddie@gmail.com>
2023-02-05 20:53:54 -0300

    Changed the book for "Bora Vender"

--- hello/views.py
+++ hello/views.py
@@ -110,20 +110,13 @@ def construct_prompt(question: str, context_embeddings: dict, df: pd.DataFrame) 
         chosen_sections.append(SEPARATOR + document_section.content)
         chosen_sections_indexes.append(str(section_index))
 
-    header = """Sahil Lavingia is the founder and CEO of Gumroad, and the author of the book The Minimalist Entrepreneur (also known as TME). These are questions and answers by him. Please keep your answers to three sentences maximum, and speak in complete sentences. Stop speaking once your point is made.\n\nContext that may be useful, pulled from The Minimalist Entrepreneur:\n"""
+    header = """Alfredo Soares, fundador da XTECH COMMERCE, que no final de 2017, foi comprada pela VTEX, maior empresa da América Latina de tecnologia de e-commerce. Também fundou a SocialRocket, plataforma de automação para Instagram que transforma perfis em canais de vendas. Com ela, em dois anos Alfredo e sua equipe chegaram a mais de 150 mil usuários cadastrados em 7 países. A obra de Alfredo Soares pode ser chamada de “um guia para o empreendedor que põe a mão na massa e vende”.\n"""
 
-    question_1 = "\n\n\nQ: How to choose what business to start?\n\nA: First off don't be in a rush. Look around you, see what problems you or other people are facing, and solve one of these problems if you see some overlap with your passions or skills. Or, even if you don't see an overlap, imagine how you would solve that problem anyway. Start super, super small."
+    question_1 = "\n\n\nQ: Vender é só para o time comercial?\n\nA: Levando em consideração que vender é gerar valor para o seu cliente, apresentando a solução que melhor atende suas necessidades, é antiquado pensar que vender é responsabilidade somente dos executivos de vendas. As empresas que desejam se destacar no mercado e construir uma verdadeira máquina de vendas precisam fazer com que toda a empresa trabalhe focada em encantar os consumidores, colocando eles no centro do processo, o que chamamos de negócios customer centric."
     question_2 = "\n\n\nQ: Q: Should we start the business on the side first or should we put full effort right from the start?\n\nA:   Always on the side. Things start small and get bigger from there, and I don't know if I would ever “fully” commit to something unless I had some semblance of customer traction. Like with this product I'm working on now!"
     question_3 = "\n\n\nQ: Should we sell first than build or the other way around?\n\nA: I would recommend building first. Building will teach you a lot, and too many people use “sales” as an excuse to never learn essential skills like building. You can't sell a house you can't build!"
-    question_4 = "\n\n\nQ: Andrew Chen has a book on this so maybe touché, but how should founders think about the cold start problem? Businesses are hard to start, and even harder to sustain but the latter is somewhat defined and structured, whereas the former is the vast unknown. Not sure if it's worthy, but this is something I have personally struggled with\n\nA: Hey, this is about my book, not his! I would solve the problem from a single player perspective first. For example, Gumroad is useful to a creator looking to sell something even if no one is currently using the platform. Usage helps, but it's not necessary."
-    question_5 = "\n\n\nQ: What is one business that you think is ripe for a minimalist Entrepreneur innovation that isn't currently being pursued by your community?\n\nA: I would move to a place outside of a big city and watch how broken, slow, and non-automated most things are. And of course the big categories like housing, transportation, toys, healthcare, supply chain, food, and more, are constantly being upturned. Go to an industry conference and it's all they talk about! Any industry…"
-    question_6 = "\n\n\nQ: How can you tell if your pricing is right? If you are leaving money on the table\n\nA: I would work backwards from the kind of success you want, how many customers you think you can reasonably get to within a few years, and then reverse engineer how much it should be priced to make that work."
-    question_7 = "\n\n\nQ: Why is the name of your book 'the minimalist entrepreneur' \n\nA: I think more people should start businesses, and was hoping that making it feel more “minimal” would make it feel more achievable and lead more people to starting-the hardest step."
-    question_8 = "\n\n\nQ: How long it takes to write TME\n\nA: About 500 hours over the course of a year or two, including book proposal and outline."
-    question_9 = "\n\n\nQ: What is the best way to distribute surveys to test my product idea\n\nA: I use Google Forms and my email list / Twitter account. Works great and is 100% free."
-    question_10 = "\n\n\nQ: How do you know, when to quit\n\nA: When I'm bored, no longer learning, not earning enough, getting physically unhealthy, etc… loads of reasons. I think the default should be to “quit” and work on something new. Few things are worth holding your attention for a long period of time."
-
-    return (header + "".join(chosen_sections) + question_1 + question_2 + question_3 + question_4 + question_5 + question_6 + question_7 + question_8 + question_9 + question_10 + "\n\n\nQ: " + question + "\n\nA: "), ("".join(chosen_sections))
+   
+    return (header + "".join(chosen_sections) + question_1 + question_2 + question_3 + "\n\n\nQ: " + question + "\n\nA: "), ("".join(chosen_sections))
 
 def answer_query_with_context(
     query: str,
@@ -146,7 +139,7 @@ def answer_query_with_context(
     return response["choices"][0]["text"].strip(" \n"), context
 
 def index(request):
-    return render(request, "index.html", { "default_question": "What is this book about?" })
+    return render(request, "index.html", { "default_question": "Qual o assunto deste livro?" })
 
 @csrf_exempt
 def ask(request):
